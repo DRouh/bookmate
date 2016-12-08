6bf197b38ffe2cfc23b4cbb416cda04cbdc70b56
Dime <dimerouh@gmail.com>
2016-12-08 12:00:54 +0300

    Averaging the answers for words that have higher "known" estimate than the one that has just been answered.

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -15,27 +15,35 @@ end
 if exist("answers.mat", 'file') == 2
   d = importdata("answers.mat");
 else
-  m = (max(count) - min(count)) / 2;
+  m = (max(count) - min(count));
   d = count / m;
 end
 
+
+%positioning figure on display
+ss = get(0,'screensize'); %The screen size
+width = ss(3);
+height = ss(4);
+H = figure;
+vert = 400; %300 vertical pixels
+horz = 550; %600 horizontal pixels
+%This will place the figure in the top-right corner
+set(H,'Position',[width-horz-50, height-vert-100, horz, vert]);
+
+
 prompt = 'How many words you would want to answer: ';
 wordCount = input(prompt);
-
+value = (m)/2;
 for i = 1:wordCount
   %plotting
-  figure(1);
   plot (count, d, "*", "markersize", 7);
   hold;
   plot (count, d);
 
   %find value closest to 0.5
   %which means uncertainty
-  value = (max(d) - min(d))/2;
   fprintf('Looking for closest to %f \n', value);
-  
   [c index] = min(abs(d - value));
-  
   fprintf('The word is "%s"\n', words{index,1});
   userAsnwered = false;
   
@@ -43,23 +51,34 @@ for i = 1:wordCount
     prompt = 'Do you know this word (1/0)? ';
     x = input(prompt);
 
-    if (x = 1) 
+    if (x == 1) 
       userAsnwered = true;
-      d(index) = 1;
-      
-    elseif (x = 0)
+      d(index) = 1;   
+    elseif (x == 0)
       userAsnwered = true;
       d(index) = 0;
-      
-    else 
-      userAsnwered = false;
     end
     
     if i ~= wordCount
       clf;
     end  
   end 
+	
+  %todo keep track of 0s and 1s because they mean a Certain answer to the question!
+  if index - 2 > 0
+    m_1 = mean(d(2: index-1))
+    upperLimit = index-1;
+    for i= 2:upperLimit
+      d(i) = m_1;
+    end
+  end  
+  
+
+
   %rebuild distibution?
+  newMinIndexOfLastKnown = min(find(d ~= 1)); 
+  newMax = max(d(newMinIndexOfLastKnown))-min(d);
+  value = (newMax)/2;
 end  
 
 %saving answers to a file
