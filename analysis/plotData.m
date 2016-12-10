98e4e102516ff74776af30ee36b70789fac87a32
Dime <dimerouh@gmail.com>
2016-12-10 09:08:24 +0300

    Experimenting with filtering

--- analysis/plotData.m
+++ analysis/plotData.m
@@ -12,11 +12,11 @@ function plotData(count, p, value, index)
   plot(x(index), p(index),'LineWidth',2, "gx", "markersize", 15)
 
   anweredPoints = find(p == 0 | p == 1);
+  sx = x(anweredPoints);
+  sy = p(anweredPoints)';
+  plot(sx, sy,'LineWidth',4, "r*", "markersize", 3);
 
-  if length(anweredPoints) >= 2
-    sx = x(anweredPoints);
-    sy = p(anweredPoints)';
-    plot(sx, sy,'LineWidth',2, "r*", "markersize", 3);
+
     %yy = spline(sx, sy, x);
     %yy
     %max(yy)
@@ -25,13 +25,19 @@ function plotData(count, p, value, index)
     %max(yz)
     %plot(x,yz);
     %plot(x, yy,'--','LineWidth', 1);
-
-    windowSize = 10;
-    b = ones(1,windowSize)/windowSize;
-    yy = filter(b, 1, sy);
-    plot(sx,yy);
-    legend ("normailzed count", "current question", "answered", "probability locus");
-  else
-    legend ("normailzed count", "current question", "answered");
-  end
+    
+    %windowSize = 10;
+    %b = ones(1,windowSize)/windowSize;
+    %yy = filter(b, 1, sy,1);
+    yy = runningAverage(sy,sy,10);
+    
+    plot(sx,yy, 'linewidth', 2);
+    if length(sx) > 5
+      yyy = sgolayfilt (sy,3,5);
+      plot(sx,yyy, 'linewidth', 2);
+      legend ("normailzed count", "current question", "answered", "running average", "Savitsky-Golay");
+    else
+      legend ("normailzed count", "current question", "answered", "running average");
+    end
+   
 endfunction
---
+++ analysis/runningAverage.m
@@ -0,0 +1,19 @@
+function yy = runningAverage(x, y, windowSize)
+    display(length(x));
+    if length(x) >= windowSize
+      n = windowSize;
+      m = (n - 1) / 2;
+      len = size(x, 2);
+      yy=x;
+      div = ones(size(x));
+      for k = 1:m
+         k2 = 2*k;   % Slightly faster
+         z  = zeros(1, k);
+         yy  = yy + [z, x(1:len - k2) + x(1 + k2:len), z];
+         div(k + 1:len - k) = div(k + 1:len - k) + 2;
+      end
+      yy = yy ./ div;
+    else
+      yy = y;  
+    end  
+endfunction
