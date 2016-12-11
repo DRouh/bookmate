132a4b51b341312b95d0d5a1356be6833de44364
Dime <dimerouh@gmail.com>
2016-12-11 17:22:15 +0300

    Tidied up plots.

--- analysis/plotData.m
+++ analysis/plotData.m
@@ -1,7 +1,8 @@
 function plotData(count, p, value, index, closestY)
   %plotting
-  
   clf;
+  subplot(2,1,1);
+  
   x = [1:length(count)];
   %all words as points
   plot (x, p, "o", "markersize", 4);
@@ -16,21 +17,30 @@ function plotData(count, p, value, index, closestY)
   sy = p(anweredPoints)';
   plot(sx, sy,'LineWidth',4, "r*", "markersize", 2);
 
-  %running average plot
-  windowSize = 5;
-  if length(sy) >= windowSize
-    yy = runningAverage(sy, sy, windowSize);
-    plot(sx,yy, 'linewidth', 2, '--');
-    
-    %fitting polynomial
-    polynY = fitPoly(length(p), sx, sy, min(windowSize, length(x)));
-    plot(sx, polynY, ":k",'LineWidth', 2)
-    
-    legend ("normailzed count", "current question", "answered", "running average", "fitted polynomial");
-  else
-    legend ("normailzed count", "current question", "answered");
-  end
-  
+  %plot where find our word
   cy = ones(1,length(x)) * closestY;
   plot(x, cy, 'linewidth', 2, '--'); 
+  
+  title('Vocabulary estimate');
+  xlabel('Indices of ranked words') % x-axis label
+  ylabel('Probability of knowing') % y-axis label
+  legend ("normailzed count", "current question", "answered", "search area");
+  %running average plot
+  
+  windowSize = 5;
+  if length(sy) >= windowSize
+    subplot(2,1,2);
+    yy = runningAverage(sy, sy, windowSize);
+    plot(sx,yy, 'linewidth', 2, '--');
+    hold;
+    %fitting polynomial
+    deg = min(windowSize, length(x));
+    polynY = fitPoly(length(p), sx, sy, deg);
+    plot(sx, polynY, ":k",'LineWidth', 2)
+    title('Vocabulary estimate');
+    xlabel('Indices of ranked words') % x-axis label
+    ylabel('Probability of knowing') % y-axis label
+    legend ("running average", "fitted polynomial");
+  end
+
 endfunction
