b97d8775f6340234a81c1513b19ef43f51d728df
Dime <dimerouh@gmail.com>
2016-12-11 17:22:49 +0300

    -Fixed routine that finds new word to test. -Fixed user input processing.

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -4,15 +4,25 @@ pkg load signal;
 %loading data (words and count), vector of probability (d) and m (mean value to consider)
 [words,count, p, v] = loadData('data.mat', 'short.txt');
 
-wordCount = input('How many words you would want to be shown: ');
-value = v / 2;
-
+%ask user how many words he wants to test
+testedWords = p(p == 0 | p == 1);
+notTestedCount = length(p) - length(testedWords);
+accepted = false;
+while ~accepted
+  wordCount = input('How many words you would want to be shown: ');
+  if wordCount > length(p) || wordCount > (length(p)-length(testedWords))
+    fprintf('We have only %d not tested.\n', notTestedCount);
+  else
+    accepted=true;
+  end
+end
+  
 %positioning figure on display
 H = prepareFigure();
 for i = 1:wordCount
   if p(end) == 0 || p(end) == 1 
     %determine where it's best to pick new word to test
-    value = getNextWordToTest(p);
+    value = getNextWordToTest(p, index);
     [c index] = findClosest(p, value);
   else
     index = length(p);
--- analysis/getNextWordToTest.m
+++ analysis/getNextWordToTest.m
@@ -1,15 +1,31 @@
-function value = getNextWordToTest(p)
-  %min - for the words with the greatest count
-  %max - for the words with the smallest count
-    
-  %find poin near which we seek next word to ask
-  %!!!if the word was true - go right, if not then go left
-  notTestedWords = p(find(p ~= 1 & p ~= 0));
-  meanP = mean(notTestedWords);
-  unansweredWordProbabilities = p(find(p ~= 1 & p ~= 0 & meanP > p)); 
+function value = getNextWordToTest(p, prevIndex)
+  %if user knew previous word - test more difficult words; otherwise - easier
+  if prevIndex < length(p)
+    if p(prevIndex) == 1
+      right = p(prevIndex:end);
+      notTestedWords = right(find(right ~= 1 & right ~= 0));
+      unansweredWordProbabilities = notTestedWords;
+    else
+      left = p(1:prevIndex);
+      notTestedWords = left(find(left ~= 1 & left ~= 0));
+      unansweredWordProbabilities = notTestedWords;
+    end
+  else
+    notTestedWords = p(find(p ~= 1 & p ~= 0));
+    meanOfNotTestedP = mean(notTestedWords);
+    unansweredWordProbabilities = notTestedWords(find(meanOfNotTestedP > notTestedWords)); 
+  end  
 
-  %minMaxDiff = abs(min(unansweredProbs)-max(unansweredProbs)); 
-  minMaxDiff2 = abs(min(unansweredWordProbabilities)-max(unansweredWordProbabilities));
+ 
+  halfWayIndex = floor(length(unansweredWordProbabilities) / 2);
+  %fix for edge cases. get to random unanswered point if got beyound limits
+  if halfWayIndex == 0
+        fprintf('Loading data....\n');
+    unansweredWordProbabilities = p(find(p ~= 1 & p ~= 0));
+    rndIndex = round(rand(1)*length(unansweredWordProbabilities))
+    value = unansweredWordProbabilities(rndIndex);
+  else  
+    value = unansweredWordProbabilities(halfWayIndex);
+  end
   
-  value = minMaxDiff2 / 2;
 endfunction    
