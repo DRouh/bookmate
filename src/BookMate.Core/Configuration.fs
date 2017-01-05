c7f92eb5e9a2bee22f28c69de5c9c52e5195bb91
DRouh <dimerouh@gmail.com>
2017-01-05 17:24:04 +0300

    Extracted application configuration to xml. Added XML-type provider that reads configuration from xml file, provided sample configuration and an empty file for the actual configuration.

--- src/BookMate.Core/Configuration.fs
+++ src/BookMate.Core/Configuration.fs
@@ -1,10 +1,21 @@
 ﻿namespace BookMate.Core
 module Configuration = 
+    open FSharp.Data
+
+    [<LiteralAttribute>]let applicationXMLSample = @"appconfiguration.sample.xml"
+
+    [<LiteralAttribute>]let applicationXMLActual = @"appconfiguration.xml"
+
+    type private XMLApplicationConfiguration = XmlProvider<Sample=applicationXMLSample, Global=true>
+
+    let currDirectory = System.IO.Directory.GetCurrentDirectory()
+    let private config = XMLApplicationConfiguration.Load(applicationXMLActual)
+    //
     //provide appropriate values
-    let getYandexTranslateApiEndPoint = ""
-    let getYandexTranslateApiKey = ""
-    let getYandexDictionaryApiEndPoint = ""
-    let getYandexDictionaryApiKey = ""
-    let getDBConnectionString = "" |> System.IO.Path.GetFullPath
-    let getUserDefinedWordsFilePath = @""
-    let getStanfordModelFolder = @""
+    let getYandexTranslateApiEndPoint = config.YandexTranslateApiEndPoint
+    let getYandexTranslateApiKey = config.YandexTranslateApiKey
+    let getYandexDictionaryApiEndPoint = config.YandexDictionaryApiEndPoint
+    let getYandexDictionaryApiKey = config.YandexDictionaryApiKey
+    let getDBConnectionString = config.DbConnectionString |> System.IO.Path.GetFullPath
+    let getUserDefinedWordsFilePath = config.UserDefinedWordsFilePath
+    let getStanfordModelFolder = config.StanfordModelFolder
