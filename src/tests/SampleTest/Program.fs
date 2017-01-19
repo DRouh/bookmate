f7a5c1c31224cf4eb126e591335ebd1965298cbf
DRouh <dimerouh@gmail.com>
2017-01-19 12:35:29 +0300

    Added initial test project

---
+++ src/tests/SampleTest/Program.fs
@@ -0,0 +1,26 @@
+﻿namespace SampleTest
+open Xunit
+open FsUnit.Xunit
+
+module CommonTest = 
+    type Point3D = {
+      x: float
+      y: float
+      z: float  
+    }
+    let (==) = LanguagePrimitives.PhysicalEquality
+    let inline (!=) a b = not (a == b)
+    
+    let a = {x = 1.0; y = 2.0; z = 3.0}
+    let b = {x = 1.0; y = 2.0; z = 3.0}
+    let c = a 
+    
+    [<Fact>]
+    let ``== operator should show physical equality from two references``() =
+        (a == b) |> should be False
+        (a == c) |> should be True
+    
+    [<Fact>]
+    let ``!= operator should show physical inequality of two references``() =
+        (a != b) |> should be True
+        (a != c) |> should be False
