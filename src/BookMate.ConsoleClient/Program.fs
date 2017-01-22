34813543c97b409e6720df52178b9af1024b2800
DRouh <dimerouh@gmail.com>
2017-01-22 15:22:27 +0300

    Cleaning up in console client

--- src/BookMate.ConsoleClient/Program.fs
+++ src/BookMate.ConsoleClient/Program.fs
@@ -21,10 +21,10 @@ module Program =
                 getBookPath [| "" |]
         | _ -> failwith "Sorry. Can't hand these input arguments."
     
-    //let chooseBookProcessor (path : string) = 
-    //    match Path.GetExtension path with
-    //    | ".epub" -> EpubProcessor.processBook
-    //    | _ -> failwith "Format isn't supported"
+    let chooseBookProcessor (path : string) = 
+        match Path.GetExtension path with
+        | ".epub" -> EpubProcessor.processBook
+        | _ -> failwith "Format isn't supported"
     
     [<EntryPoint>]
     let main argv = 
@@ -32,7 +32,5 @@ module Program =
         //let processBook = chooseBookProcessor bookPath 
         //do processBook bookPath
 
-        let paragraph = "Mr. & Mrs. Smith is a 2005 American romantic comedy action film. The film stars Brad Pitt and Angelina Jolie as a bored upper-middle class married couple. They are surprised to learn that they are both assassins hired by competing agencies to kill each other.";
-        printfn "%A" paragraph
         System.Console.Read() |> ignore
         0
