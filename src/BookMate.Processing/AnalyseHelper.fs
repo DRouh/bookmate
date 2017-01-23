e4bcb8769d449b3450163b6689f3da277471a65c
DRouh <dimerouh@gmail.com>
2017-01-23 10:03:09 +0300

    Developing tests for Processing module

--- src/BookMate.Processing/AnalyseHelper.fs
+++ src/BookMate.Processing/AnalyseHelper.fs
@@ -6,29 +6,23 @@ module AnalyseHelper =
     open POS
     open Analyse
 
-    let getPosTagger () : Tagger = raise (System.NotImplementedException "Not ready")
-    let getTokenizer () : Tokenizer = raise (System.NotImplementedException "Not ready")
+    let getPosTagger () : Tagger = fun _ -> raise (System.NotImplementedException "Not ready")
+    let getTokenizer () :  Tokenizer = fun _ -> raise (System.NotImplementedException "Not ready")
 
-    let tagText (textToTags: Tagger) text =
-        let tokens = textToTags text
-        tokens
+    let tagWords' (textToTags: Tagger) = textToTags >> Array.ofSeq 
 
-    let tagWords text = 
-        let tagText = tagText (getPosTagger())
-        let taggedWords = text |> tagText
-        taggedWords |> Array.ofSeq
-    
-    let getWordStats text = 
-        let taggedWords = tagWords text
-        let stats =
-             taggedWords
-             |> Array.Parallel.map (fun (word, pos) -> (word.ToLower(), pos))
-             |> Array.groupBy (fun (word, _) -> word)
-             |> Array.Parallel.map (fun (word, group) -> 
-                                                let poss = group |> Array.map (fun (w,p) -> p)
-                                                (word, poss, group.Length))
-             |> Array.sortByDescending (fun (_, _, c) -> c)
-        stats
+    let tagWords = tagWords' (getPosTagger())
+
+    let computeWordPosStat = 
+        Array.where (fun (word:string, pos:CommonPoS) -> isNotNull word)
+        Array.Parallel.map (fun (word:string, pos) -> (word.ToLower(), pos))
+        >> Array.groupBy (fun (word, _) -> word)
+        >> Array.Parallel.map (fun (word, group) -> 
+                                        let poss = group |> Array.map (fun (w,p) -> p)
+                                        (word, poss, group.Length))
+        >> Array.sortByDescending (fun (_, _, c) -> c)
+
+    let getWordStats = tagWords >> computeWordPosStat
     
     let textToWords omitPunctuation text = 
         let tokenizeText = getTokenizer() omitPunctuation
--- tests/BookMate.Processing.Tests/AnalyseTests.fs
+++ tests/BookMate.Processing.Tests/AnalyseTests.fs
@@ -3,8 +3,24 @@ namespace BookMate.Processing.Tests
 module AnalyseTests = 
     open Xunit
     open FsUnit.Xunit
-    open System.Reflection
+    open BookMate.Processing.POS
+    open BookMate.Processing.Analyse
+    open BookMate.Processing.AnalyseHelper
+
 
     [<Fact>]
-    let ``Test of Test`` () =
+    let ``Words, part of speech pairs should return correct statistics`` () =
+      let testData = [|("test", Noun); ("test", Verb); ("cat", Noun)|]
+      let expected = [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|]
+      let actual = computeWordPosStat testData
+      expected = actual |> should be True
+
+
+
+    [<Fact>]
+    let ``Edge cases`` () =
+      let testData = [|(null, Noun); ("", Verb); ("  ", Noun)|]
+   //   let expected = [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|]
+      let actual = computeWordPosStat testData
+      printfn "%A" actual
       1 = 1 |> should be True
