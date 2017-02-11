5aa7d85cc62264bf8e791baed37a8ee604c2b2c5
DRouh <dimerouh@gmail.com>
2017-02-11 10:22:15 +0300

    Alter build script for better CI support

--- build.cmd
+++ build.cmd
@@ -1,30 +1,41 @@
 dotnet restore
 
-dir 
-
+@echo off
 cd src
 
+@echo off
 cd BookMate.Core
 IF NOT EXIST "appconfiguration.json" copy NUL "appconfiguration.json"
 dotnet build
 
+@echo off
 cd..
 cd BookMate.Integration
 dotnet build
 
+@echo off
 cd..
 cd BookMate.NLPServices
 dotnet build
 
+@echo off
 cd..
 cd BookMate.Processing
 dotnet build
 
+@echo off
 cd..
 cd BookMate.Web/server
 dotnet build
+@echo off
 cd..
 
 cd..
+@echo off
 cd BookMate.ConsoleClient
 dotnet build
+
+@echo off
+cd..
+@echo off
+cd..
