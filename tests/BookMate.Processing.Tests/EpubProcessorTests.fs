c115acba643910442d9d4d7823190ce1ec837862
DRouh <dimerouh@gmail.com>
2017-02-18 13:04:37 +0300

    Create initial test for future EpubProcessor

---
+++ tests/BookMate.Processing.Tests/EpubProcessorTests.fs
@@ -0,0 +1,10 @@
+namespace BookMate.Processing.Tests
+
+module EpubProcessorTests = 
+    open Xunit
+    open FsUnit.Xunit
+    
+    [<Fact>]
+    let ``1 = 1``() = 
+        1 = 1 |> should be True
+    
