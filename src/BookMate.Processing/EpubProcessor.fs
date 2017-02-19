3b204bc7b73ad872f4da975e39cfb083a67a6692
DRouh <dimerouh@gmail.com>
2017-02-19 20:32:34 +0300

    Initial tests for read book function

--- src/BookMate.Processing/EpubProcessor.fs
+++ src/BookMate.Processing/EpubProcessor.fs
@@ -1,9 +1,26 @@
 namespace BookMate.Processing
 
-module EpubProcessor =
-    
+module EpubProcessor = 
+    open System
+    open System.IO
     open BookMate.Processing.Epub
     open BookMate.Processing.EpubIO
-
-    // type BookToProcess = {Files: seq<EpubFile>; Location: PackDirPath}
-    // and EpubFile = { name: string; path: EpubFilePath; content: string}
+    
+    type BookToProcess = 
+        { Files : seq<EpubFile>
+          Location : UnpackedPath }
+    
+    and EpubFile = 
+        { name : string
+          path : EpubFilePath
+          content : string }
+    
+    let readBook (unpackedBook : UnpackedPath) : BookToProcess = 
+        let UnpackedPath (EpubFilePath filePath, UnpackedDirPath dirPath) = unpackedBook
+        { Files = 
+              [ { name = ""
+                  path = EpubFilePath ""
+                  content = "" } ]
+          Location = unpackedBook }
+    //let contentFiles = Directory.GetFiles(dirPath, "*.*html", System.IO.SearchOption.AllDirectories)
+    //{Files = [{name= ""; path = x; content = ""}]}
--- tests/BookMate.Processing.Tests/EpubProcessorTests.fs
+++ tests/BookMate.Processing.Tests/EpubProcessorTests.fs
@@ -1,19 +1,30 @@
 namespace BookMate.Processing.Tests
 
 module EpubProcessorTests = 
+    open System
+    open System.IO
+
     open Xunit
     open FsUnit.Xunit
+    open BookMate.Processing.Epub
     open BookMate.Processing.EpubIO
     open BookMate.Processing.EpubProcessor
-    (*  To do items
-        1. Read file
-            1.1. Unpack file
-            1.2. Read file into memory
-        2. Process file
-        3. Save file  
-    *)
+
+    let sampleDirectory = Path.Combine(Directory.GetCurrentDirectory(), "SampleData")
+    let sampleFile = Directory.GetFiles(sampleDirectory, "*.epub").[0]
 
     [<Fact>]
-    let ``1 = 1``() = 
-        1 = 1 |> should be True
+    let ``Read book should have valid location data``() = 
+        let fileName = Path.GetFileNameWithoutExtension(sampleFile)
+        let saveDirName = sprintf "%s_%s" fileName (Guid.NewGuid().ToString())
+        let saveDirPath = Path.Combine(sampleDirectory, saveDirName)
+
+        let unpackedBook = unpackBook (sampleFile |> toFilePath |> Option.get) (saveDirPath |> toPackDirPath |> Option.get) |> Option.get
+        
+        do Directory.Delete(saveDirPath, true)
+        
+        let actual = readBook unpackedBook
+
+        actual.Location = unpackedBook |> should be True
+
     
