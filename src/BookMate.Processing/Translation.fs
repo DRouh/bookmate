45dbb02543c0fad55d0d7796f84f9179e4baa296
DRouh <dimerouh@gmail.com>
2017-03-05 13:53:28 +0300

    Refactor Translation module

--- src/BookMate.Processing/Translation.fs
+++ src/BookMate.Processing/Translation.fs
@@ -4,7 +4,7 @@ module Translation =
   open System.Text.RegularExpressions
   open BookMate.Core.Helpers.RegexHelper
   open BookMate.Processing.POS
-  open BookMate.Processing.Epub.Domain
+    open BookMate.Processing.Epub.Domain
   let words (text : string) = 
       [ let mutable start = 0
         let mutable index = 0
@@ -20,9 +20,9 @@ module Translation =
   let unwords = List.reduce (+)
   let (=~) target regex = System.Text.RegularExpressions.Regex.IsMatch(target, regex, RegexOptions.IgnoreCase)
   let isIn xs y = List.contains y xs
-  let doIntersectWith (xs : 'a list) (ys : 'a list) = xs |> List.exists (fun x -> List.contains x ys)
+  let doesIntersectWith xs ys = xs |> List.exists (fun x -> List.contains x ys)
   let toTranslatedForm original translation = original + "{" + translation + "}"
-  let toString (xs : string list) = xs |> String.concat ","
+  let toString xs = xs |> String.concat ","
 
   let toGroupedTranslation translations = 
       let normalizeTranslation = function 
@@ -38,35 +38,40 @@ module Translation =
           |> List.map mapOriginalToAllTranslations
       translationGroupedForWord
 
-  let pickTranslationsForPos translations tags = 
-      translations
-      |> List.where (fun (_, tag) -> tag |> isIn tags)
+  let forTagTranslations wordTranslationAndTags wordTags = 
+      wordTranslationAndTags
+      |> List.where (fun (_, tag) -> tag |> isIn wordTags)
+      |> List.map (fst)
+      |> toString
+
+  let pickTranslationsForPos originalWord originalWordTags allWordTranslations forTagTranslations wordTags = 
+      let translation = 
+          if wordTags |> doesIntersectWith originalWordTags then forTagTranslations
+          else allWordTranslations
+      translation |> toTranslatedForm originalWord
+
+  let toWordTaggedOccurrences taggedWordsInSentence word = 
+      taggedWordsInSentence
+      |> Array.ofList
+      |> Array.filter (fun (currentWord, _) -> currentWord =~ word)
+      |> Array.map (fun (_, tags) -> tags)
+
+  let toTranslationsList wordTranslationAndTags = 
+      wordTranslationAndTags
       |> List.map (fst)
       |> toString
 
   let applyWordTranslations taggedWordsInSentence text (originalWord, wordTranslationAndTags) = 
       let tagsForOriginalWord = wordTranslationAndTags |> List.map (snd)
+      let allTranslationsForWord = wordTranslationAndTags |> toTranslationsList
+      let taggedWordOccurrences = originalWord |> toWordTaggedOccurrences taggedWordsInSentence
       
-      let allTranslationsForWord = 
-          wordTranslationAndTags
-          |> List.map (fst)
-          |> toString
-      
-      let taggedWordOccurrences = 
-          taggedWordsInSentence
-          |> Array.ofList
-          |> Array.filter (fun (w, _) -> w =~ originalWord)
-          |> Array.map (fun (_, tags) -> tags)
-      
-      let pickTranslations word c = 
-          if word =~ originalWord then 
-              let wordTags = taggedWordOccurrences.[c - 1]
-              if wordTags |> doIntersectWith tagsForOriginalWord then 
-                  wordTags
-                  |> pickTranslationsForPos wordTranslationAndTags
-                  |> toTranslatedForm word
-              else allTranslationsForWord |> toTranslatedForm word
-          else word
+      let pickTranslations (currentWord, count) = 
+          if currentWord =~ originalWord then 
+              let wordTags = taggedWordOccurrences.[count - 1]
+              let forTagTranslations = forTagTranslations wordTranslationAndTags wordTags
+              wordTags |> pickTranslationsForPos currentWord tagsForOriginalWord allTranslationsForWord forTagTranslations
+          else currentWord
       
       let applyTranslations words = 
           let runningCountOccurrences (_, acc) word = 
@@ -76,17 +81,19 @@ module Translation =
               (word, count)
           words
           |> List.scan runningCountOccurrences ("", 0)
-          |> List.map (fun (word, count) -> pickTranslations word count)
+          |> List.map pickTranslations
       
       text
       |> words
       |> applyTranslations
       |> unwords
 
-  let applyTranslations (taggedWords : TaggedWord list) (translations : Translation list) (text : string) = 
-      let translationGroupedForWord = toGroupedTranslation translations
+  let applyTranslations taggedWords translations text = 
       let applyWordTranslations' = taggedWords |> applyWordTranslations
-      translationGroupedForWord |> List.fold applyWordTranslations' text
+      translations
+      |> toGroupedTranslation
+      |> List.fold applyWordTranslations' text
+      
   let translateText tagWords lookup matcher wordsToTranslate text = 
       let taggedWords = tagWords text //todo refactor and pass as an arguments
       let translations = lookup text //todo refactor and pass as an arguments
