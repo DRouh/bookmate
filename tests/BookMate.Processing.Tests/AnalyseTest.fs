541d0550af777a96cdc042b61652ddad16ec43e2
DRouh <dimerouh@gmail.com>
2017-03-05 08:19:43 +0300

    Clean up tests

--- tests/BookMate.Processing.Tests/AnalyseTest.fs
+++ tests/BookMate.Processing.Tests/AnalyseTest.fs
@@ -9,21 +9,15 @@ module AnalyseTest =
 
     [<Fact>]
     let ``Words, part of speech pairs should return correct statistics`` () =
-      let testData = [|("test", Noun); ("test", Verb); ("cat", Noun)|]
-      let expected = [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|]
-      let actual = computeWordPosStat testData
-      expected = actual |> should be True
+      let actual = computeWordPosStat [|("test", Noun); ("test", Verb); ("cat", Noun)|]
+      [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|] |> should equal actual
 
     [<Fact>]
     let ``Edge cases should be correctly handled in statistics`` () =
-      let testData = [|(null, Noun);("", Verb); ("  ", Noun)|]
-      let expected = [|("", [| Verb |], 1); ("  ", [| Noun |], 1)|]
-      let actual = computeWordPosStat testData
-      expected = actual |> should be True
-
+      let actual = computeWordPosStat [|(null, Noun);("", Verb); ("  ", Noun)|]
+      [|("", [| Verb |], 1); ("  ", [| Noun |], 1)|] |> should equal actual
+      
     [<Fact>]
     let ``Empty array of input pairs should result in empty statistics`` () =
-      let testData :(string*CommonPoS)[]= [||]
-      let expected :(string*CommonPoS[]*int)[]= [||]
-      let actual = computeWordPosStat testData
-      expected = actual |> should be True
+      let actual = computeWordPosStat [||]
+      [||] |> should equal actual
