0215e42fad62e600b79df0d431cf36f0ca51f77b
DRouh <dimerouh@gmail.com>
2017-03-09 09:29:39 +0300

    Rearrange test assertions

--- tests/BookMate.Processing.Tests/AnalyseTest.fs
+++ tests/BookMate.Processing.Tests/AnalyseTest.fs
@@ -10,14 +10,14 @@ module AnalyseTest =
     [<Fact>]
     let ``Words, part of speech pairs should return correct statistics`` () =
       let actual = computeWordPosStat [|("test", Noun); ("test", Verb); ("cat", Noun)|]
-      [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|] |> should equal actual
+      actual  |> should equal [|("test", [| Noun; Verb |], 2); ("cat", [| Noun |], 1)|]
 
     [<Fact>]
     let ``Edge cases should be correctly handled in statistics`` () =
       let actual = computeWordPosStat [|(null, Noun);("", Verb); ("  ", Noun)|]
-      [|("", [| Verb |], 1); ("  ", [| Noun |], 1)|] |> should equal actual
+      actual |> should equal [|("", [| Verb |], 1); ("  ", [| Noun |], 1)|]
       
     [<Fact>]
     let ``Empty array of input pairs should result in empty statistics`` () =
       let actual = computeWordPosStat [||]
-      [||] |> should equal actual
+      actual |> should equal [||]
